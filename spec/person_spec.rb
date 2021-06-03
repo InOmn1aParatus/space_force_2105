fc976acdeed0446cf27d95798c54aaf2ea5e2914
Matt Kragen <mattdkragen@gmail.com>
2021-06-03 08:29:19 -0700

    Create test for adding specialty

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -14,4 +14,15 @@ RSpec.describe Person do
       expect(kathy.specialties).to eq([])
     end
   end
+
+  describe 'methods' do
+    it 'adds a specialty' do
+      kathy = Person.new('Kathy Chan', 10)
+      kathy.add_specialty(:astrophysics)
+      kathy.add_specialty(:quantum_mechanics)
+
+      expect(kathy.specialties).to eq([:astrophysics, :quantum_mechanics])
+    end
+
+  end
 end
