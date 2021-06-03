7914acc4b9f24c6cb65c0bd52f7b89da4640fe0d
Matt Kragen <mattdkragen@gmail.com>
2021-06-03 08:13:43 -0700

    Correct test for Person instance

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe Person do
   describe 'instantiation' do
     it 'exists' do
       kathy = Person.new('Kathy Chan', 10)
-      expect(kathy),to be_a(Person)
+      expect(kathy).to be_a(Person)
     end
+  end
 end
