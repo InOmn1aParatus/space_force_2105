99b528e130037f55e1618127b925b173954dc117
Matt Kragen <mattdkragen@gmail.com>
2021-06-03 08:25:40 -0700

    Create attribute tests for Spacecraft and Person

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -6,5 +6,12 @@ RSpec.describe Person do
       kathy = Person.new('Kathy Chan', 10)
       expect(kathy).to be_a(Person)
     end
+
+    it 'has attributes' do
+      kathy = Person.new('Kathy Chan', 10)
+      expect(kathy.name).to eq('Kathy Chan')
+      expect(kathy.experience).to eq(10)
+      expect(kathy.specialties).to eq([])
+    end
   end
 end
