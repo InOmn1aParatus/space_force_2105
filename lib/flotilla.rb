08eb52d52e472ee3902b92a08aa0bf45c0212673
Matt Kragen <mattdkragen@gmail.com>
2021-06-03 08:59:41 -0700

    Correct spec indentation; create #add_ship method

--- lib/flotilla.rb
+++ lib/flotilla.rb
@@ -1,9 +1,13 @@
 class Flotilla
   attr_reader :name, :personnel, :ships
-  
+
   def initialize(details)
     @name = details[:designation]
     @personnel = []
     @ships = []
+  end
+
+  def add_ship(ship)
+    @ships << ship
   end
 end
--- spec/flotilla_spec.rb
+++ spec/flotilla_spec.rb
@@ -18,10 +18,12 @@ RSpec.describe Flotilla do
   end
 
   describe 'methods' do
-    daedalus = Spacecraft.new({name: 'Daedalus', fuel: 400})
-    seventh_flotilla = Flotilla.new({designation: 'Seventh Flotilla'})
-    seventh_flotilla.add_ship(daedalus)
+    it 'adds ships'do
+      daedalus = Spacecraft.new({name: 'Daedalus', fuel: 400})
+      seventh_flotilla = Flotilla.new({designation: 'Seventh Flotilla'})
+      seventh_flotilla.add_ship(daedalus)
 
-    expect(seventh_flotilla.ships).to eq([daedalus])
+      expect(seventh_flotilla.ships).to eq([daedalus])
+    end
   end
 end
